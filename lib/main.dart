eb64957d5b19d6a7ed3cf87251e915bbe59bbc5c
Jathu 23 <jathushanj2003@gmail.com>
2025-02-06 20:22:58 +0530

    get input

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
-  
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String inputdata = "";
+  @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Column(
           children: [
-           Row(
-                children: [
-                       calbutton("7", Colors.grey),
-                       calbutton("8", Colors.grey),
-                       calbutton("9", Colors.grey),
-                       calbutton("/", Colors.grey),
-                ],
-           ),
-              Row(
-                children: [
-                       calbutton("4", Colors.grey),
-                       calbutton("5", Colors.grey),
-                       calbutton("6", Colors.grey),
-                       calbutton("*", Colors.grey),
-                ],
-           ),
-              Row(
-                children: [
-                       calbutton("1", Colors.grey),
-                       calbutton("2", Colors.grey),
-                       calbutton("3", Colors.grey),
-                       calbutton("-", Colors.grey),
-                ],
-           ),
-              Row(
-                children: [
-                       calbutton("0", Colors.grey),
-                       calbutton(".", Colors.grey),
-                       calbutton("=", Colors.grey),
-                       calbutton("+", Colors.grey),
-                ],
-           )
-                
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                inputdata.toString(),
+                style: TextStyle(color: Colors.white, fontSize: 100),
+              ),
+            ),
+            Row(
+              children: [
+                calbutton("7", Colors.grey),
+                calbutton("8", Colors.grey),
+                calbutton("9", Colors.grey),
+                calbutton("/", Colors.orange),
+              ],
+            ),
+            Row(
+              children: [
+                calbutton("4", Colors.grey),
+                calbutton("5", Colors.grey),
+                calbutton("6", Colors.grey),
+                calbutton("*", Colors.orange),
+              ],
+            ),
+            Row(
+              children: [
+                calbutton("1", Colors.grey),
+                calbutton("2", Colors.grey),
+                calbutton("3", Colors.grey),
+                calbutton("-", Colors.orange),
+              ],
+            ),
+            Row(
+              children: [
+                calbutton("0", Colors.grey),
+                calbutton(".", Colors.grey),
+                calbutton("=", Colors.orange),
+                calbutton("+", Colors.orange),
+              ],
+            )
           ],
         ),
       ),
@@ -57,17 +66,25 @@ class MainApp extends StatelessWidget {
   }
 
   Widget calbutton(String text, Color bgcolor) {
-    return Container(
-      decoration: BoxDecoration(
-        color: bgcolor,
-        borderRadius: BorderRadius.circular(100),
-      ),
-      height: 40,
-      width: 40,
-      alignment: Alignment.center,
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.white54, fontSize: 10),
+    return InkWell(
+      onTap: () {
+        setState(() {
+          inputdata += text; 
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: bgcolor,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        height: 80,
+        width: 80,
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
       ),
     );
   }
